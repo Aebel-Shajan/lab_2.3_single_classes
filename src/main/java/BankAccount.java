733e808d7b80f2e0d223dcfc222416f992970937
Aebel Shajan <aebelshajan@gmail.com>
2024-02-07 14:29:10 +0000

    created BankAccount class with getters and setters.

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,47 @@
+public class BankAccount {
+    String firstName;
+    String lastName;
+    String dateOfBirth;
+    int accountNumber;
+    float balance;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+}
